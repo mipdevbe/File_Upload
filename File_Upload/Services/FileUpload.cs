8f14943d26c1f8460ec123d6b77fabece191044e
Michel <michel.philippe@festijazz.be>
2025-04-02 12:56:41 +0200

    Initial update

---
+++ File_Upload/Services/FileUpload.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace File_Upload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if (file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+
+                    // Create the uploads folder if it does not exist
+                    var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    using (var stream = file.OpenReadStream())
+                    {
+                        await using (var fileStream = new FileStream(uploadPath, FileMode.Create))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+                    }
+
+                    _logger.LogInformation($"File uploaded successfully: {file.Name}");
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
